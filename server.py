693bb9964a4be367504fac8be8cd3920c87c4817
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-14 01:42:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,3 +9,4 @@ def handle_client(client_socket,client_address):
         print(f"Connection closed by {client_address}")
         break
       print(f"[{client_address[0]}:{client_address[1]}]: {message}")
+      broadcast(message, client_socket)
