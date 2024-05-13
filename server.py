402cbc15c35b6b0c391badc4fe1d589173d575dc
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-14 01:33:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,3 +2,9 @@ import socket
 import threading
 def handle_client(client_socket,client_address):
   print(f"Connected with {client_address}")
+  while True:
+    try:
+      message = client_socket.recv(1024).decode('utf-8')
+      if not message:
+        print(f"Connection closed by {client_address}")
+        break
