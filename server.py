beef9417463829020997ba8676822f0dd8c07e55
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-16 21:11:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import socket
 import threading
+
 def handle_client(client_socket, client_address):
     print(f"Connected with {client_address}")
+
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
@@ -15,7 +17,10 @@ def handle_client(client_socket, client_address):
             break
 
     client_socket.close()
+
 def broadcast(message, sender_socket):
+    for client in clients:
+        if client != sender_socket:
             try:
                 client.send(message.encode('utf-8'))
             except:
@@ -24,19 +29,23 @@ def broadcast(message, sender_socket):
 
 def remove_client(client):
     if client in clients:
-        remove_client(client)
+        clients.remove(client)
 
 def main():
     host = socket.gethostbyname(socket.gethostname())
     port = 5555
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen()
+
     print(f"Server is listening for incoming connections on {host}:{port}")
+
     while True:
         client_socket, client_address = server_socket.accept()
         clients.append(client_socket)
-        client_thread = thread.Thread(target = handle_client, args = (client_socket, client_address))
+
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
         client_thread.start()
 
 if __name__ == "__main__":
