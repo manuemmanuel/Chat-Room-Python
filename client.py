64309b6bdb4edcc13560d8b217ce46bc634d9d7b
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-18 00:33:41 +0530

    Update client.py

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ def receive_messages(client_socket):
       message = client_socket.recv(1024).decode('utf-8')
       print("\n[Server]:",message)
     except:
-      
+      print("Connection to the server closed.")
 
 def send_messages(client_socket):
   while True:
