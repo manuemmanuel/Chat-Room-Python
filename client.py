7cdfc904041e7807c656e916e6b9323c13aeca3b
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-19 00:16:32 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,4 +3,7 @@ import threading
 
 def receive_messages(client_socket):
   while True:
-    
+    try:
+      message = client_socket.recv(1024).decode('utf-8')
+      if not message:
+        print("Connection to the server closed.")
